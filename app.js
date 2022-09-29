6c597d92695bf52fd5ce853d86a871a3be178a8c
jdoming8 <jillian_dominguez@brown.edu>
2022-09-29 18:43:23 -0400

    bakery done

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const apiRequest = async () => {
     }
   });
 
-  // console.log(response);
+   console.log(response);
 
   // Return the response in JSON format
   return response.json();
@@ -26,14 +26,19 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
+  //find name of all fruits whose calories are less than 150
 
+  const newArr = fruitsArray.filter(fruit => fruit.nutritions.calories <50)
   // TODO: Create a new HTML element to display your data
-
+  console.log(newArr)
   // TODO: Append your new element to the page
+  const newElement = document.createElement('div');
+  newElement.innerHTML = newArr.map(fruit => fruit.name)
+  gallery.append(newElement)
 
 }
 
